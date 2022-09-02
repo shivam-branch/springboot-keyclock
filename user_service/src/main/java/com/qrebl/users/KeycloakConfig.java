5bcb9fc3f3abd65179f6de70c4564f8593ea6aa6
shivam-branch <shivam.tripathi@unthinkable.co>
2022-09-02 09:10:49 +0530

    keyclock authentication

---
+++ user_service/src/main/java/com/qrebl/users/KeycloakConfig.java
@@ -0,0 +1,36 @@
+package com.qrebl.users;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+
+public class KeycloakConfig {
+    static Keycloak keycloak = null;
+    final static String serverUrl = "http://localhost:8080/auth";
+    public final static String realm = "snacks";
+    final static String clientId = "royal_cliff";
+    final static String clientSecret = "b79faaf0-c352-4155-91cb-785e588aeb7b";
+    final static String userName = "admin";
+    final static String password = "admin";
+
+
+    public KeycloakConfig() {
+    }
+
+    public static Keycloak getInstance(){
+        if(keycloak == null){
+
+            keycloak = KeycloakBuilder.builder()
+                    .serverUrl(serverUrl)
+                    .realm(realm)
+                    .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                    .username(userName)
+                    .password(password)
+                    .clientId(clientId)
+                    .clientSecret(clientSecret)
+                    .build();
+        }
+        return keycloak;
+    }
+}
